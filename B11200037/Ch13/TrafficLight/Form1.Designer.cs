b6d9a7653a297844cc7ae2ac9f5b4eb61113b693
Hi0s <163806284+Hi0s@users.noreply.github.com>
2025-05-16 21:03:39 +0800

    Add files via upload

---
+++ B11200037/Ch13/TrafficLight/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TrafficLight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Traffic_light_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Traffic_light_lbl
+            // 
+            this.Traffic_light_lbl.BackColor = System.Drawing.Color.Red;
+            this.Traffic_light_lbl.Location = new System.Drawing.Point(120, 226);
+            this.Traffic_light_lbl.Name = "Traffic_light_lbl";
+            this.Traffic_light_lbl.Size = new System.Drawing.Size(174, 161);
+            this.Traffic_light_lbl.TabIndex = 0;
+            this.Traffic_light_lbl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Traffic_light_lbl_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 648);
+            this.Controls.Add(this.Traffic_light_lbl);
+            this.Name = "Form1";
+            this.Text = "TrafficLight";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Traffic_light_lbl;
+    }
+}
+
---
+++ B11200037/Ch13/TrafficLight/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficLight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Traffic_light_lbl_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    Traffic_light_lbl.BackColor = Color.Yellow;
+                    break;
+                case MouseButtons.Right:
+                    Traffic_light_lbl.BackColor = Color.Green;
+                    break;
+            }
+        }
+    }
+}
