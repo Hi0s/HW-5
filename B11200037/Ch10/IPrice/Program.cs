7fd12d6f949af721228bb25ad8d292887f152f22
Hi0s <163806284+Hi0s@users.noreply.github.com>
2025-05-16 21:01:44 +0800

    Add files via upload

---
+++ B11200037/Ch10/IPrice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+interface IPrice {
+    double GetPrice();
+}
+class Car : IPrice { public double Price;
+    public string? Name;
+    public double GetPrice(){ return this.Price; }
+    public string? GetName() { return  this.Name; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Price = 10000;
+        car.Name = "Toyota";
+        Console.WriteLine(
+            $"Car Name: {car.GetName()}\n" +
+            $"Car Price: {car.GetPrice()}");
+    }
+}
